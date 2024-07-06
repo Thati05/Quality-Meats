faacb95b53ebd8a49db91d6df6b1aada454a7b20
Thati05 <m.seithati01@gmail.com>
2024-07-06 23:16:54 +0200

    commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,6 +3,13 @@ import hero from '../assets/hero.png'
 import logo from '../assets/logo.svg'
 import aboutimg from '../assets/aboutimg.png'
 import image from '../assets/image.png'
+import chicken from '../assets/chicken.png'
+import goat from '../assets/goat.png'
+import sheep from '../assets/sheep.png'
+import pig from '../assets/pig.png'
+import cow from '../assets/cow.png'
+
+
 
 
 export {
@@ -10,6 +17,11 @@ export {
   logo,
   hero,
   aboutimg,
-  image
+  image,
+  sheep,
+  goat,
+  pig,
+  chicken,
+  cow,
 
 }
